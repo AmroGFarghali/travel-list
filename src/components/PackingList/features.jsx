f3ecb1336d09acf2ec2e3861804d5f07cb9c932d
AmrGFarghali <lordmooora@gmail.com>
2024-06-19 18:33:20 +0300

    removed comments

--- src/components/PackingList/features.jsx
+++ src/components/PackingList/features.jsx
@@ -9,13 +9,6 @@ export const useTravelListFeatures = () => {
         item.id === id ? { ...item, checked: !item.checked } : item
       )
     );
-    // const packedItem = packedItems?.find((item) => item?.id === id);
-    // const itemIPack = items.find((item) => item?.id === id);
-    // packedItem
-    //   ? setPackedItems((currItems) =>
-    //       currItems.filter((currItem) => currItem !== packedItem)
-    //     )
-    //   : setPackedItems((currItems) => [...currItems, itemIPack]);
   };
   const handleDelete = (id) => {
     setItems((currItems) => currItems.filter((item) => item.id !== id));
