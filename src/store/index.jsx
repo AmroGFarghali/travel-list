5f9b3c98cc1b942926b6e5a71e7f2b9bfe335008
AmrGFarghali <lordmooora@gmail.com>
2024-06-19 18:25:39 +0300

    added session storage

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,20 @@
 import create from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useTravelStore = create((set) => ({
-  items: [],
+// export const useTravelStore = create((set) => ({
+//   items: [],
 
-  setItems: (items) => set({ items }),
-}));
+//   setItems: (items) => set({ items }),
+// }));
+export const useTravelStore = create(
+  persist(
+    (set) => ({
+      items: [],
+      setItems: (items) => set({ items }),
+    }),
+    {
+      name: "travel-storage", // unique name for the storage
+      getStorage: () => localStorage, // (optional) by default localStorage is used
+    }
+  )
+);
