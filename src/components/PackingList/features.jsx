2d711f9b5c6f4bc2724aa488544132a88e902391
AmrGFarghali <lordmooora@gmail.com>
2024-06-23 10:08:35 +0300

    added sorting and different rendering states

--- src/components/PackingList/features.jsx
+++ src/components/PackingList/features.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useTravelStore } from "../../store";
 
 export const useTravelListFeatures = () => {
   const { items, setItems } = useTravelStore();
+  const [selectedSortMethod, setSelectedSortMethod] = useState("input");
 
+  let sortedItems;
   const handleCheck = (id) => {
     setItems(
       items.map((item) =>
@@ -11,15 +14,29 @@ export const useTravelListFeatures = () => {
     );
   };
   const handleDelete = (id) => {
-    setItems((currItems) => currItems.filter((item) => item.id !== id));
+    setItems(items.filter((item) => item.id !== id));
   };
 
   const handleClear = () => {
     setItems([]);
   };
+  const handleSort = (e) => {
+    setSelectedSortMethod(e.target.value);
+  };
+
+  if (selectedSortMethod === "input") sortedItems = items;
+  if (selectedSortMethod === "name")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.itemName.localeCompare(b.itemName));
+  if (selectedSortMethod === "packed")
+    sortedItems = items.slice().sort((a, b) => b.checked - a.checked);
 
   return {
+    selectedSortMethod: [selectedSortMethod, setSelectedSortMethod],
+    sortedItems,
     handleCheck,
+    handleSort,
     handleClear,
     handleDelete,
   };
--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -3,8 +3,22 @@ import { useTravelStore } from "../../store";
 export default function Stats() {
   const { items } = useTravelStore();
 
-  const packedItems = items.filter((item) => item.checked !== false).length;
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
 
+  const packedItems = items.filter((item) => item.checked !== false).length;
+  const packedPercentage = (packedItems / items.length) * 100 || 0;
+
+  if (packedPercentage === 100)
+    return (
+      <p className="stats">
+        <em>You have packed everything in your list!</em>
+      </p>
+    );
   return (
     <div className="stats">
       {" "}
